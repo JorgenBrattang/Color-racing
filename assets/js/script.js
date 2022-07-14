0e385f7c6608b402c5cea56b56762e91040767b7
JorgenBrattang <jorgen.kvist@gmail.com>
2022-07-14 07:25:29 +0000

    Add eventlistener to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,24 @@
-const myButtons = document.getElementById('wrapper-button');
-
-myButtons.addEventListener('click', (event) => {
-  const isButton = event.target.nodeName === 'BUTTON';
-  if (!isButton) {
-    return;
-  }
-  console.log(event.target.id);
-})
-
-
-
-const playButton = document.getElementById("play--btn");
-playButton.addEventListener("click", function() {
-  document.getElementById("play--btn").style.backgroundColor = 'red';
-});
-
-const color1Button = document.getElementById("color-1--btn");
-color1Button.addEventListener("click", function() {
-  document.getElementById("color-1--btn").style.backgroundColor = 'red';
-});
-
-const color2Button = document.getElementById("color-2--btn");
-color2Button.addEventListener("click", function() {
-  document.getElementById("color-2--btn").style.backgroundColor = 'red';
-});
-
-const color3Button = document.getElementById("color-3--btn");
-color3Button.addEventListener("click", function() {
-  document.getElementById("color-3--btn").style.backgroundColor = 'red';
-});
-
-const color4Button = document.getElementById("color-4--btn");
-color4Button.addEventListener("click", function() {
-  document.getElementById("color-4--btn").style.backgroundColor = 'red';
-});
+let buttons = document.getElementsByTagName("button");
+let i = 1;
+for (button of buttons) {
+    button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "color-1") { 
+            console.log('color 1');
+        }
+        else if (this.getAttribute("data-type") === "color-2") {
+            console.log('color 2');
+        }
+        else if (this.getAttribute("data-type") === "play") {
+            console.log('Play');
+        }
+        else if (this.getAttribute("data-type") === "color-3") {
+            console.log('color 3');
+        }
+        else if (this.getAttribute("data-type") === "color-4") {
+            console.log('color 4');
+        }
+        else {
+            alert("button doesn't choose a data-type, look that up!")
+        }
+    })
+}
