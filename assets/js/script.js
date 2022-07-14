0e351a69825f458494bd5d62431e200aa145e29d
JorgenBrattang <jorgen.kvist@gmail.com>
2022-07-14 09:46:22 +0000

    Scrap old js code to an improved eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,84 +3,37 @@ const color2 = '#A96C7B';
 const color3 = '#E38C79';
 const color4 = '#FFBC6C';
 const playColor = '#00A97F';
-const selectColor = '#C9C9C9';
+const hoverColor = '#C9C9C9';
+const selectColor = '#393939';
 
-let buttons = document.getElementsByTagName("button");
-let i = 1;
-for (button of buttons) {
-    button.addEventListener("click", function(event) {
-        if (this.getAttribute("data-type") === "color-1") { 
-            select(event, color1);
-        }
-        else if (this.getAttribute("data-type") === "color-2") {
-            select(event, color2);
-        }
-        else if (this.getAttribute("data-type") === "play") {
-            select(event, playColor);
-        }
-        else if (this.getAttribute("data-type") === "color-3") {
-            select(event, color3);
-        }
-        else if (this.getAttribute("data-type") === "color-4") {
-            select(event, color4);
-        }
-        else {
-            alert("button doesn't choose a data-type, look that up!")
-        }
-    })
+const button = document.querySelectorAll('button')
 
-    button.addEventListener("mouseover", function(event) {
-        if (this.getAttribute("data-type") === "color-1") { 
-            mouseOver(event);
-        }
-        else if (this.getAttribute("data-type") === "color-2") {
-            mouseOver(event);
-        }
-        else if (this.getAttribute("data-type") === "play") {
-            mouseOver(event);
-        }
-        else if (this.getAttribute("data-type") === "color-3") {
-            mouseOver(event);
-        }
-        else if (this.getAttribute("data-type") === "color-4") {
-            mouseOver(event);
-        }
-        else {
-            alert("button doesn't choose a data-type, look that up!")
-        }
-    })
+addGlobalEventListener("click", "button", e => {
+    console.log(e)
+    console.log(e.target)
+    e.target.style.backgroundColor = 'red';
+})
 
-    button.addEventListener("mouseleave", function(event) {
-        if (this.getAttribute("data-type") === "color-1") { 
-            mouseLeave(event, color1);
-        }
-        else if (this.getAttribute("data-type") === "color-2") {
-            mouseLeave(event, color2);
-        }
-        else if (this.getAttribute("data-type") === "play") {
-            mouseLeave(event, playColor);
-        }
-        else if (this.getAttribute("data-type") === "color-3") {
-            mouseLeave(event, color3);
-        }
-        else if (this.getAttribute("data-type") === "color-4") {
-            mouseLeave(event, color4);
-        }
-        else {
-            alert("button doesn't choose a data-type, look that up!")
-        }
+/**
+ * Type: "click" 
+ * 
+ * selector: "button"
+ * 
+ * callback: e =>
+ * 
+ * Then you can call the e with:
+ * 
+ * console.log(e)
+ * 
+ * To Check what you need to change.
+ * 
+ * Example: 
+ * 
+ * e.target.style.backgroundColor = 'red';
+ */
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e => {
+        if (e.target.matches(selector)) callback(e)
     })
 }
 
-function select(event, color) {
-
-    event.target.style.backgroundColor = color;
-}
-
-function mouseOver(event) {
-    event.target.style.backgroundColor = selectColor;
-}
-
-function mouseLeave(event, color) {
-    event.target.style.backgroundColor = color;
-}
