c6af4d422ead6139fad72f9e3f3c0a02bb857eeb
JorgenBrattang <jorgen.kvist@gmail.com>
2022-07-15 07:37:31 +0000

    Issue found, can't get targetID outside of function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,41 +9,69 @@ const backColor = '#393939'
 /* Gets all the buttons from DOM */
 // const button = document.querySelectorAll('button') 
 
-
+/* Selects all the buttons (<a>) in the DOM */
 const button = document.querySelectorAll('.select-container > a') 
+/* Gets the play button and sets it to it's default Icon */
 const selectPlay = document.getElementById('play')
 selectPlay.innerHTML = '<i class="fa-solid fa-play"></i>'
+
+/* Gets the horses from the HTML */
+const getColor_0 = document.getElementById("racer_0")
+
+        
+/* Lets the horseWidth to be 1% of 100%, so you can see where your horse starts */
+let colorHeight_0 = 10;
+getColor_0.style.height = colorHeight_0 + "%";
+
+let e = null
 
 addGlobalEventListener("click", '.select-container > a', e => { 
     let targetID = e.target.id
     let numbers = [0,1,2,3]
-
-    /* If the targetID is true, do that. Otherwise take away the ID from the array. */
-    if(targetID == 'play') {
-        selectPlay.innerHTML = '<i class="fa-solid fa-person-praying"></i>'
-        selectPlay.style.backgroundColor = waitColor
-        selectPlay.style.color = backColor
-    } else {
-        numbers.splice(targetID, 1)
-    }
-    console.log('Your id is: ' + targetID)
-    
-
-    /* Gets the right color from the targetID */
     let color = 'color_' + targetID
-        e.target.style.backgroundColor = color
+    e.target.style.backgroundColor = color
 
-    /* If it isn't the play button, change the colors of the others. */
     if(targetID != 'play') {
+        numbers.splice(targetID, 1)
         for (number of numbers) {
             targetOthers = document.getElementById(number)
             targetOthers.style.opacity = "0.5";
         }
-        e.target.style.cssText = 'background-color: ' + color
+        e.target.style.cssText = 'background-color: ' + color 
+        
+        /* I want targetID to get outside of the function */
+        let colorID = []
+        colorID.push(targetID)
+        console.log('This is inside: ' + colorID)
     }
 
-    
+    return colorID
 })
+
+console.log('This is outside: ' + colorID)
+
+function runGame() {
+        selectPlay.innerHTML = '<i class="fa-solid fa-person-praying"></i>'
+        selectPlay.style.backgroundColor = waitColor
+        selectPlay.style.color = backColor
+
+        /* Sets the interval of the time from the random number for each horse */
+        let setColor_0 = setInterval(a, 50);
+
+        /**
+         * Gets the interval speed for Brown horse and if its not 100% keep going.
+         */
+        function a() {
+            /* Checks if the length of the race is finished (100%) */
+            if (colorHeight_0 >= 100) {
+                clearInterval(setColor_0);
+            } else {
+                /* if it isn't finished, keep on going. */
+                colorHeight_0++;
+                getColor_0.style.height = colorHeight_0 + "%";
+            }
+        }
+}
 
 /**
  * This function add eventListener to your code. 
