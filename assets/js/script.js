391055f7054b51a7823e4294a52939bcd46dc150
JorgenBrattang <jorgen.kvist@gmail.com>
2022-07-16 17:18:05 +0000

    Bug found, after reset one color is still 100% opacity

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,13 @@ const color_3 = '#FFBC6C'
 const waitColor = '#C9C9C9'
 const textColor = '#393939'
 const resetColor = '#8685EF'
-
 /* Selects all the buttons (<a>) in the DOM */
 const button = document.querySelectorAll('.select-container > a') 
 /* Selects all the class="first--box" etc (<div>) in the DOM */
 const colorFinish = document.querySelectorAll('.placement-container > div > div')
-
 /* Gets the play button and sets it to it's default Icon */
 const selectPlay = document.getElementById('play')
 selectPlay.innerHTML = '<i class="fa-solid fa-play"></i>'
-
 /* Sets a global variable for colorID, so we can reuse it outside. */
 var colorID = []
 
@@ -24,7 +21,6 @@ addGlobalEventListener("click", '.select-container > a', e => {
     let targetID = e.target.id
     const colorNumbers = [0,1,2,3]
     const colorSelect = 'color_' + targetID
-
     /* Checks if you pressed a color instead of play button */
     if(targetID != 'play') {
         /* Pushes the id of the color into an array */
@@ -39,20 +35,29 @@ addGlobalEventListener("click", '.select-container > a', e => {
         /* Changes the background color of the current chosen color */
         e.target.style.cssText = 'background-color: ' + colorSelect 
     }    
-
     /* Checks if colorID has to many ID's within the array. */
     if (colorID.length > 1) {
         colorID.shift(); // removes the first element from an array 
     } 
-
     /* Checks if the colorID holds an ID or not. */
     if(colorID.length == 1) {
         if(targetID == 'play') {
             runGame()
-            
         }
     } else {
         alert('Choose a color to race with first, then press the big green button!')
+    }
+
+    if(targetID == 'reset') {
+        /* Deselects the color chosen */
+        colorID = []
+        document.getElementById("reset").setAttribute("id", "play")
+        const buttonNumber = [0,1,2,3,'play']
+        for (id of buttonNumber) {
+            document.getElementById(id).style.pointerEvents = 'auto'
+        }
+        selectPlay.innerHTML = '<i class="fa-solid fa-play"></i>'
+        console.log(targetID)
     }
 })
 
@@ -60,16 +65,14 @@ function runGame() {
     /* This will disable all click events on the divs */
     const buttonNumber = [0,1,2,3,'play']
     for (id of buttonNumber) {
-        document.getElementById(id).style.pointerEvents = 'none';
+        document.getElementById(id).style.pointerEvents = 'none'
     }
     // Create a function/loop between this ---------->>>
-
     /* Gets the colors from the HTML */
     const getColor_0 = document.getElementById("racer_0")
     const getColor_1 = document.getElementById("racer_1")
     const getColor_2 = document.getElementById("racer_2")
     const getColor_3 = document.getElementById("racer_3")
-
     /* Lets the colorHeight to be 10% of 100%, so you can see where your colors starts */
     let colorHeight_0 = 10;
     getColor_0.style.height = colorHeight_0 + "%";
@@ -79,9 +82,7 @@ function runGame() {
     getColor_2.style.height = colorHeight_2 + "%";
     let colorHeight_3 = 10;
     getColor_3.style.height = colorHeight_3 + "%";
-    
     // Create a function/loop between this <<<----------
-
     /* Adds the praying person icon and changes the play button color */
     selectPlay.innerHTML = '<i class="fa-solid fa-person-praying"></i>'
     selectPlay.style.backgroundColor = waitColor
@@ -100,18 +101,13 @@ function runGame() {
         }
     // create 4 numbers for the array
     } while (randomNumberArray.length < 4)
-
-
     // Create a function/loop between this ---------->>>
-    
     /* Sets the interval of the time from the random number for each horse */
     let setColor_0 = setInterval(a, randomNumberArray[0])
     let setColor_1 = setInterval(b, randomNumberArray[1])
     let setColor_2 = setInterval(c, randomNumberArray[2])
     let setColor_3 = setInterval(d, randomNumberArray[3])
-
     const winnerArray = []
-
     /**
      * Gets the interval speed for colors and checks if its not 100% keep going.
      */
@@ -127,7 +123,6 @@ function runGame() {
             getColor_0.style.height = colorHeight_0 + "%"
         }
     }
-
     /**
      * Gets the interval speed for colors and checks if its not 100% keep going.
      */
@@ -143,7 +138,6 @@ function runGame() {
             getColor_1.style.height = colorHeight_1 + "%"
         }
     }
-
     /**
      * Gets the interval speed for colors and checks if its not 100% keep going.
      */
@@ -159,7 +153,6 @@ function runGame() {
             getColor_2.style.height = colorHeight_2 + "%"
         }
     }
-
     /**
      * Gets the interval speed for colors and checks if its not 100% keep going.
      */
@@ -179,7 +172,6 @@ function runGame() {
 }
 
 function winnerColor(winnerArray) {
-
     if(winnerArray.length === 1) {
         colorFinish[0].style.cssText = 'background-color: ' + winnerArray[0]
         if(color_0 == winnerArray[0]) {
@@ -194,7 +186,6 @@ function winnerColor(winnerArray) {
         else if(color_3 == winnerArray[0]) {
             announceWinner(3)
         }
-        
     } 
     else if (winnerArray.length === 2) {
         colorFinish[1].style.cssText = 'background-color: ' + winnerArray[1]
@@ -205,19 +196,20 @@ function winnerColor(winnerArray) {
     else if (winnerArray.length === 4) {
         colorFinish[3].style.cssText = 'background-color: ' + winnerArray[3]
 
+        /* This will enable click event on the div */
+        document.getElementById('play').style.pointerEvents = 'auto'
         
-        /* This will enable all click events on the divs */
-        const buttonNumber = [0,1,2,3,'play']
-        for (id of buttonNumber) {
-            document.getElementById(id).style.pointerEvents = 'auto';
-        }
-
         selectPlay.innerHTML = '<i class="fa-solid fa-rotate-left"></i>'
         selectPlay.style.backgroundColor = resetColor
         selectPlay.style.color = textColor
+
+        const colorNumbers = [0,1,2,3]
+        for (number of colorNumbers) {
+            document.getElementById(number).style.opacity = "1"
+        }
+        /* Changes the play buttons ID to reset */
+        document.getElementById("play").setAttribute("id", "reset")
     }
-
-
 }
 
 function announceWinner(winID) {
